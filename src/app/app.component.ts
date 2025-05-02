b0e4ab976d45b95993a7d5facfca48c6ee6c612f
Koenig2005 <s241562@student.dhbw-mannheim.de>
2025-05-02 16:19:13 +0200

    💄Erste Button-Komponente erstellt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { EnvironmentHandler } from "../environments/handler/environment.handler";
 import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { ButtonComponent } from "./components/button/button.component";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, ButtonComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
