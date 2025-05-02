dddd74e56dd102e2972e9b3651b95c5ede3d4fa0
Vigan Veliu <vigan.veliu@litexa.de>
2025-05-02 11:19:21 +0200

    Fixed not creating client multiple times

--- src/services/supabase.service.ts
+++ src/services/supabase.service.ts
@@ -11,15 +11,19 @@ import { environment } from "../environments/environment.production";
  * See https://supabase.com/docs/reference/javascript/start
  */
 export class SupabaseService {
-  private readonly supabaseClient: SupabaseClient;
+  private readonly supabaseClient?: SupabaseClient;
 
   constructor() {
+    if (this.supabaseClient) {
+      return;
+    }
+
     this.supabaseClient = createClient(environment.supabaseUrl!, environment.supabaseKey!);
   }
 
   // Example usage for signing up a user.
-  public signUpUser(email: string, password: string): Promise<AuthResponse> {
-    return this.supabaseClient.auth.signUp({
+  public signUpUser(email: string, password: string): Promise<AuthResponse> | undefined {
+    return this.supabaseClient?.auth.signUp({
       email: email,
       password: password,
       options: {
