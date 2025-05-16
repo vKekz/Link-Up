2f209ee411185cfb55aaf89190e6324cb49b5796
Koenig2005 <s241562@student.dhbw-mannheim.de>
2025-05-16 15:25:40 +0200

    Großartiger Commit mit großartiger Sidebar Action

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from "@angular/router";
 import { UserRegistrationFormComponent } from "./components/user-registration-form/user-registration-form.component";
 import { AppComponent } from "./app.component";
 import { UserLoginFormComponent } from "./components/user-login-form/user-login-form.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
 
 export const routes: Routes = [
   { path: "", component: AppComponent, pathMatch: "full" },
+  { path: "sidebar", component: SidebarComponent, pathMatch: "full"},
   { path: "register", component: UserRegistrationFormComponent, pathMatch: "full" },
   { path: "login", component: UserLoginFormComponent, pathMatch: "full" },
 ];
