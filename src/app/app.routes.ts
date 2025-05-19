21bd07e32b6e183682a79c8206b0c1bed04d02fe
49152 <lauado@students.uni-mainz.de>
2025-05-19 10:25:42 +0200

    Landing Page erstellt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from "@angular/router";
 import { UserRegistrationFormComponent } from "./components/user-registration-form/user-registration-form.component";
 import { AppComponent } from "./app.component";
 import { UserLoginFormComponent } from "./components/user-login-form/user-login-form.component";
+import { LandingPageComponent } from "./components/landing-page/landing-page.component";
 
 export const routes: Routes = [
   { path: "", component: AppComponent, pathMatch: "full" },
   { path: "register", component: UserRegistrationFormComponent, pathMatch: "full" },
   { path: "login", component: UserLoginFormComponent, pathMatch: "full" },
+  { path: "landing", component: LandingPageComponent, pathMatch: "full" },
 ];
