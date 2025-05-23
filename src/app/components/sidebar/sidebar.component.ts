35be66e1b80e101ba2d0cccfac6fc3ae5c1cf43b
Koenig2005 <s241562@student.dhbw-mannheim.de>
2025-05-23 12:15:14 +0200

    ✨ Sidebar fertig

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  standalone: true,
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
 
+  constructor(protected readonly router: Router) {}
+
+  isCollapsed = false;
+
+  protected toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+    const sidebarElement = document.getElementById('sidebarBox');
+    if (sidebarElement) {
+      if (this.isCollapsed) {
+        sidebarElement.classList.add('collapsed');
+      } else {
+        sidebarElement.classList.remove('collapsed');
+      }
+    }
+  }
 }
